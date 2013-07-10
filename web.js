3adde0266eef741f6b777856332c22694d25f2af
Gustavo Ferrufino <gustavo.fdlf@gmail.com>
2013-07-10 06:23:55 -0400

    Corrected typo

--- web.js
+++ web.js
@@ -2,11 +2,11 @@ var express = require('express');
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
-var buf_contents = fs.readFileSync('index.html','utf-8');
-var str_contents = buf_contents.toString();
+var ToBuffer= fs.readFileSync('index.html','utf-8');
+var strContent =ToBuffer.toString();
 
 app.get('/', function(request, response) {
-  response.send(str_contents);
+  response.send(strContent);
 });
 
 var port = process.env.PORT || 5000;
